5682695e60b926dd099b76978dad1fef9a6397f6
patrickstocklin <patrickstocklin@gmail.com>
2019-09-05 23:06:26 -0400

    added basic axios call to backend for present collections

--- src/library.js
+++ src/library.js
@@ -5,12 +5,31 @@ import axios from 'axios'
 class Library extends Component {
   constructor() {
     super();
+
+    this.state = {
+      collections: 'Collections To Be Discovered'
+    }
+    this.getCollections = this.getCollections.bind(this);
   }
+
+  getCollections() {
+    axios.get('/collection/all')
+      .then(response => this.setState({
+              collections: response.data
+            })
+      );
+  }
+
 
   render () {
     return (
         <div className='library-container' >
           <h1>Library</h1>
+          <br></br>
+          <button onClick={this.getCollections}>
+            Discover Collections
+          </button>
+          <p>{this.state.collections}</p>
         </div>
     )
   }
