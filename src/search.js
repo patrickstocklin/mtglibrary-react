53c8e2324a247bb5216ede79ef2e51dbf5d34757
patrickstocklin <patrickstocklin@gmail.com>
2019-09-16 19:53:06 -0400

    comment

--- src/search.js
+++ src/search.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './search.css'
 import axios from 'axios'
 
+//Need to break this up into many components
 class Search extends Component {
   constructor() {
     super();
